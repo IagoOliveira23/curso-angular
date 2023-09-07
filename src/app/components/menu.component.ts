052e7e58d22435da18480148cbd31bd87d787917
IagoOliveira23 <iago.ishift@gmail.com>
2023-09-07 09:01:33 -0300

    criando a estrutura de um componente

---
+++ src/app/components/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'menu-component',
+    template: '<h2>Iago</h2>',
+    styles: ['h2{color: grey}']
+})
+
+export class MenuComponent{
+
+}
